618cd51a13586e6ede31e5b963bd762937479295
Bernard Konan <devwithbernard@gmail.com>
2023-06-02 18:02:10 +0000

    declare a constant in javascript

--- 1-fundamentals/2-variables/app.js
+++ 1-fundamentals/2-variables/app.js
@@ -5,3 +5,23 @@ let message;
 message = "Hello, world!";
 
 console.log(`message: ${message}`);
+
+// Declaration de plusieurs variables
+let user="John Doe",
+    age = 23,
+    user_message = "Peace is lovely wonderful.";
+
+console.log(`
+    user: ${user}
+    age:${age}
+    message: ${user_message}
+`);
+
+// Constantes
+const RED_COLOR = "#FF0000";
+const ORANGE_COLOR  = "#ffa500";
+
+console.log(`
+    Favourite color : ${ORANGE_COLOR}
+    Hex Red Color: ${RED_COLOR}
+`)
