73a28ee40c819971efda1a83d09039f5c4c54de6
Bernard Konan <devwithbernard@gmail.com>
2023-06-28 07:59:58 +0000

    Local variable

--- 1-fundamentals/11-functions/index.js
+++ 1-fundamentals/11-functions/index.js
@@ -14,3 +14,12 @@ function showMessage() {
 
 showMessage();// execute function
 
+// Local variable
+function alertMessage() {
+    let message = "I love javascript, the best web programming language!";
+    console.log(message);
+}
+
+alertMessage();
+
+
