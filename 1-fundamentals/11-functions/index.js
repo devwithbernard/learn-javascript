674c008411c8dda07bd36151006d25d22739ba71
Bernard Konan <devwithbernard@gmail.com>
2023-06-28 07:53:52 +0000

    function declaration

---
+++ 1-fundamentals/11-functions/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/*
+* Functions in javascript
+* */
+
+console.log((function () {
+}).constructor === Function);
+
+// Declare a function
+function showMessage() {
+    console.log("Hi everyone!");
+}
+
+showMessage();// execute function
+
