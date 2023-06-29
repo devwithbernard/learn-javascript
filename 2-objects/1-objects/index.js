2daf6d9a89c4cd34120ba2362b83ab9d56f7f83f
Bernard Konan <devwithbernard@gmail.com>
2023-06-29 16:09:03 +0000

    Objects in javascript

--- 2-objects/1-objects/index.js
+++ 2-objects/1-objects/index.js
@@ -51,3 +51,52 @@ console.log(student);
 // delete square braces property
 delete student["love sport"];
 console.log(student);
+
+// Computed property
+/*let fruit = prompt("Which fruit to buy?", 'apple');
+let bag = {
+    [fruit]: 5,
+}
+alert(bag[fruit]);*/
+
+// Property value shorthand
+
+function makeUser(name, age) {
+    return {
+        name: name,
+        age: age
+    }
+}
+
+const bKonan = makeUser("Bernard Konan", 27);
+console.log(bKonan);
+
+// Another way to rewrite above function
+const makeUserCopy = (name, age) => {
+    return {name, age};
+}
+
+const johnDoe = makeUserCopy("John Doe", 30);
+console.log(johnDoe);
+
+//  Property existence test with 'in'
+user = {
+    name: "Bkonan",
+    age: 27
+}
+
+if ("name" in user) console.log("user.name exists!");
+if (!("blablabla" in user)) console.log("user.blablabla doesn't exists");
+
+// The 'for...in' loop
+let post = {
+    userId: 1,
+    id: 1,
+    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+}
+
+for (let key in post) {
+    console.log(post[key] + "\n")
+}
+
