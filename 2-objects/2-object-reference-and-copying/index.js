7b2849169c89478cbb53d0f9624d06e6a342a80b
Bernard Konan <devwithbernard@gmail.com>
2023-06-30 09:45:47 +0000

    Copying by referenceÃ

--- 2-objects/2-object-reference-and-copying/index.js
+++ 2-objects/2-object-reference-and-copying/index.js
@@ -15,4 +15,19 @@ phrase = "Hi everyone!";
 console.log("New Phrase : ", phrase);
 console.log("Message : ", message);
 
+// Copy by reference
+let user = {
+    name: "John Doe",
+    age: 30,
+}
+
+let student = user;
+
+console.log("User: ", user);
+console.log("Student: ", student);
+
+user.isAdmin = true;
+
+console.log("New User: ", user);
+console.log("New Student", student);
 
