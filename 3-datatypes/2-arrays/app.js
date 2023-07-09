3b9575ab1ab1930d21bac287b529713487a357f7
Bernard Konan <devwithbernard@gmail.com>
2023-07-09 23:42:36 +0000

    get's started with array

---
+++ 3-datatypes/2-arrays/app.js
@@ -0,0 +1,17 @@
+// Declaration
+let arr = [];
+
+// Accessing the elements
+let fruits = ["Apple", "Cherry", "Pineapple"];
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+// Replace element
+fruits[3] = "Lemon";
+console.log(fruits);
+
+// Get last element
+let lastElement = fruits.at(-1);
+console.log(lastElement);
