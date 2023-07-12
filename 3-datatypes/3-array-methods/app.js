40061e687f7106dbed297dddd70434347564867b
Bernard Konan <devwithbernard@gmail.com>
2023-07-12 15:08:00 +0000

    array.forEach and array.indexOfÃ

--- 3-datatypes/3-array-methods/app.js
+++ 3-datatypes/3-array-methods/app.js
@@ -21,3 +21,26 @@ let arr = [1, 2];
 console.log(arr.concat([3, 4]));
 console.log([...arr].concat(3));
 console.log(arr.concat([3, 4], [5, 6]));
+
+// array.forEach
+
+function log(item) {
+    console.log(item)
+}
+
+["Bilbo", "Gandalf", "Nazgul"].forEach(log);
+
+let users = ["John", "Doe", "Jean"];
+users.forEach((user, userIndex, users) => {
+    console.log(`'${user}' is at index '${userIndex}' in '${users}'`)
+})
+
+// Searching
+let nums = [1, 2, 3, 4, 5];
+
+// array.indexOf
+let isFound = nums.indexOf(2, 0);
+
+isFound === -1 ? console.log(`2 is not in nums`) : console.log("2 is in nums");
+console.log(nums.indexOf(6)); // Not found
+
